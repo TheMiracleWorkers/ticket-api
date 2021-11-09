a48b4b30b5ab5d2f5f618fce1962ce8f9eca5515
dmeuwissen <derekmeuwissen@gmail.com>
2021-11-09 17:01:56 +0100

    Changed priority choices to IntegerChoices class

--- api/models.py
+++ api/models.py
@@ -4,11 +4,10 @@ from rest_framework_jwt.serializers import User
 class Project(models.Model):
     name = models.CharField(max_length=60)
 
-PRIORITY_CHOICES = (
-    ('HIGH', '1'),
-    ('MEDIUM', '2'),
-    ('LOW', '3'),
-    )
+class PRIORITY_CHOICES(models.IntegerChoices):
+        HIGH = 1
+        MEDIUM = 2
+        LOW = 3       
 
 class Ticket(models.Model):
     title = models.CharField(max_length=250)
@@ -16,7 +15,7 @@ class Ticket(models.Model):
     due_date = models.DateTimeField(null=True)
     project = models.ForeignKey(
         Project, on_delete=models.CASCADE, null=True, related_name='tickets', related_query_name='ticket')
-    priority = models.IntegerField(max_length=10, choices=PRIORITY_CHOICES, default ='1')
+    priority = models.IntegerField(choices=PRIORITY_CHOICES.choices, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
 
